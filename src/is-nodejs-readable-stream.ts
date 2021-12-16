0549563f05035f78b945d34bce693deafd81f256
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-16 08:16:09 +0000

    chore: upgrade dependencies

--- src/is-nodejs-readable-stream.ts
+++ src/is-nodejs-readable-stream.ts
@@ -2,7 +2,7 @@ import { isObject, isBoolean, isFunction } from '@blackglory/types'
 
 export function isNodeJSReadableStream(val: any): val is NodeJS.ReadableStream {
   return isObject(val)
-      && isBoolean((val as any).readable)
-      && isFunction((val as any).read)
-      && isFunction((val as any).pipe)
+      && isBoolean(val.readable)
+      && isFunction(val.read)
+      && isFunction(val.pipe)
 }
--- src/is-nodejs-writable-stream.ts
+++ src/is-nodejs-writable-stream.ts
@@ -2,6 +2,6 @@ import { isObject, isBoolean, isFunction } from '@blackglory/types'
 
 export function isNodeJSWritableStream(val: any): val is NodeJS.WritableStream {
   return isObject(val)
-      && isBoolean((val as any).writable)
-      && isFunction((val as any).write)
+      && isBoolean(val.writable)
+      && isFunction(val.write)
 }
